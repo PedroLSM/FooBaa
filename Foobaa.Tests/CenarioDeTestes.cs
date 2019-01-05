e9df822ed778bb061e59723c4109e93edc6a5941
PedroLSM <pedrolucas.smatias@hotmail.com>
2019-01-05 14:33:22 -0200

    FooBaa, TDD exemplo first commit

---
+++ Foobaa.Tests/CenarioDeTestes.cs
@@ -0,0 +1,66 @@
+﻿using TDDExemplo;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Foobaa.Tests
+{
+    [TestClass]
+    public class CenarioDeTestes
+    {
+        [TestMethod]
+        public void QuandoPassar3RetornarFoo()
+        {
+            // Arrange
+            ConversorFooBaa cfb = new ConversorFooBaa();
+            int num = 3;
+
+            //Act
+            string resultado = cfb.ConverterMutiplo(num);
+
+            //Assert
+            Assert.AreEqual("foo", resultado);
+        }
+
+        [TestMethod]
+        public void QuandoPassar5RetornarBaa()
+        {
+            // Arrange
+            ConversorFooBaa cfb = new ConversorFooBaa();
+            int num = 5;
+
+            //Act
+            string resultado = cfb.ConverterMutiplo(num);
+
+            //Assert
+            Assert.AreEqual("baa", resultado);
+        }
+
+        [TestMethod]
+        public void QuandoPassar15RetornarFooBaa()
+        {
+            // Arrange
+            ConversorFooBaa cfb = new ConversorFooBaa();
+            int num = 15;
+
+            //Act
+            string resultado = cfb.ConverterMutiplo(num);
+
+            //Assert
+            Assert.AreEqual("foobaa", resultado);
+        }
+
+        [TestMethod]
+        public void QuandoPassar7RetornarNada()
+        {
+            // Arrange
+            ConversorFooBaa cfb = new ConversorFooBaa();
+            int num = 7;
+
+            //Act
+            string resultado = cfb.ConverterMutiplo(num);
+
+            //Assert
+            Assert.AreEqual("", resultado);
+        }
+        
+    }
+}
---
+++ TDDExemplo/ConversorFooBaa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDExemplo
+{
+    public class ConversorFooBaa
+    {
+        public string ConverterMutiplo(int num)
+        {
+            if (num % 3 == 0 && num % 5 == 0) return "foobaa";
+
+            if(num % 3 == 0) return "foo";
+
+            if (num % 5 == 0) return "baa";
+
+            return "";
+        }
+    }
+}
